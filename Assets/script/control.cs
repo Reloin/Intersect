ccc639458829a80a7935f265874df0bcfd602a8a
Reloin <yihengleong@gmail.com>
2018-04-13 22:17:08 +0800

    Initial commit

---
+++ Assets/script/control.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+	public int xDivider = 62;
+	public int yDivider = 72;
+
+	private Vector3 mousePos;
+
+	void OnMouseDrag ()
+	{
+		float mouseX = Input.mousePosition.x / xDivider;
+		float mouseY = Input.mousePosition.y /yDivider;
+		mousePos = new Vector3(mouseX, mouseY, 0);
+		transform.position = mousePos;
+
+	}
+
+	void OnMouseUp ()
+	{
+		int mouseX = Mathf.RoundToInt(Input.mousePosition.x / xDivider);
+		int mouseY = Mathf.RoundToInt(Input.mousePosition.y /yDivider);
+		mousePos = new Vector3(mouseX, mouseY, 0);
+		transform.position = mousePos;
+
+	}
+
+}
